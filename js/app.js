76abed03bfca011091ce36e57a744ffbfb30de47
KevinKYeh <71478999+KevinKYeh@users.noreply.github.com>
2021-05-28 04:49:10 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ var app = new Vue({
       if(window.location.hash){
         this.hasArgs = true
       }
-      //https://id.twitch.tv/oauth2/token?client_id=uo6dggojyb8d6soh92zknwmi5ej1q2&client_secret=nyo51xcdrerl8z9m56w9w6wg&grant_type=client_credentials
+
       var XML = new XMLHttpRequest();
             
       XML.open("POST", "https://id.twitch.tv/oauth2/token?client_id=txe9if6h2jfb6vz9d6gf76u969uhua&client_secret=n967c2dchypjejm16gn6umoeapdskj&grant_type=client_credentials");

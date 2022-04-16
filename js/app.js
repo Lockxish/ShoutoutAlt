97fd5dd134acfcddf87f78860c856e8d1431d59d
KevinKYeh <71478999+KevinKYeh@users.noreply.github.com>
2022-04-16 02:42:57 -0700

    exclude tako from so command

--- js/app.js
+++ js/app.js
@@ -154,7 +154,7 @@ var app = new Vue({
 		console.log("playlist off");
 	}
 
-    if( (flags.broadcaster || flags.mod) && (command === "soclip"  || (command === "so" && (broadcast_name != "kaeyay" && broadcast_name != "rennslyaer"))) && (playlistOn === false)) {
+    if( (flags.broadcaster || flags.mod) && (command === "soclip"  || (command === "so" && (broadcast_name != "kaeyay" && broadcast_name != "rennslyaer" && broadcast_name != takoronron))) && (playlistOn === false)) {
 	    
       message = message.toLowerCase()
       message = message.replace("@", "")

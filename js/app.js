d68c8e3451dd7ed810c73438d5cdcb5738a9579a
Lockxish <85085615+Lockxish@users.noreply.github.com>
2022-07-10 17:36:38 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ var app = new Vue({
         if(this.username == ""){
           alert("Please enter a username!")
         } else {
-          this.generatedLink = "https://kevinkyeh.github.io/ShoutoutAlt/#" + this.username
+          this.generatedLink = "https://lockxish.github.io/ShoutoutAlt/#" + this.username
           if(this.onlyme){
             this.generatedLink = this.generatedLink + "&onlyme"
           }
